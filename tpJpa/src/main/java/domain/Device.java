8414d38b82d91766436e9b608e095358acd7cbfd
13003506 <quang532@gmail.com>
2017-01-17 09:40:52 +0100

    delete all warnings

--- tpJpa/src/main/java/domain/Device.java
+++ tpJpa/src/main/java/domain/Device.java
@@ -2,7 +2,6 @@ package domain;
 
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
